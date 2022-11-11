a9f72fee511195d221ec9ae62fad46d787f6d55d
Emmanuel Canto Vazquez <emikogears134@gmail.com>
2022-11-11 12:18:33 -0600

    walletconnect provider added.

--- app/App.tsx
+++ app/App.tsx
@@ -8,16 +8,27 @@
  * @format
  */
 
+import React from 'react';
+
 import NavigationFlow from '@core/routes/navigation-flow';
 import theme from '@core/theme';
 import AuthProvider from '@core/utils/providers/auth-provider';
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import { WalletConnectProvider } from '@walletconnect/react-native-dapp/dist/providers';
 import { ThemeProvider } from 'styled-components/native';
+
+import '@core/config/shim';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const AppProvider = ({ children }) => (
   <ThemeProvider theme={theme}>
-    <AuthProvider>{children}</AuthProvider>
+    <WalletConnectProvider
+      storageOptions={{
+        asyncStorage: AsyncStorage,
+      }}
+    >
+      <AuthProvider>{children}</AuthProvider>
+    </WalletConnectProvider>
   </ThemeProvider>
 );
 
