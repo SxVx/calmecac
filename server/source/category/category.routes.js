242b75f8d23ffef9cf443c4d99c7abdd2617b196
José Enrique <drilosv13@gmail.com>
2022-11-13 09:44:51 -0600

    feat:add integration blokness

--- server/source/category/category.routes.js
+++ server/source/category/category.routes.js
@@ -26,6 +26,29 @@ router.put(
   CategoryController.updatePreferences,
 );
 
+const sdk = require('api')('@blokness/v1.0#5clkdm1elabon1uq');
+
+router.get('/x', async (req, res) => {
+  const x = await sdk
+    .nftDetails({
+      collection_smart_contract_address: '0xe583b581d5B294B3ddCC7DaAcfbe7afC56516E4c',
+      token_id: '22045FAB-5B44-4A43-A9D4-95D4F58DE046',
+      'x-api-key': 'BLK.pxBOd7gi1FzvYoKaHR9yV1Mp4Ujr9ALl4Mqj2PzwPgnjxT9tK/kgYbMHbhrpC5ww',
+    })
+    .then(({ data }) =>{
+       return data
+      },
+
+      )
+    .catch((err) => {
+
+      console.error(err)
+    });
+
+
+  res.json({ x });
+});
+
 router.get('/user-preferences', verifyToken, CategoryController.userPreferences);
 
 module.exports = router;
